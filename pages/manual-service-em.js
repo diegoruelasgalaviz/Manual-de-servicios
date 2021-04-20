9236ebc61d3d5623c46359fa5335a4b6be7dd832
Diego Ruelas <diego.ruelas@live.com>
2021-04-20 10:24:11 -0500

    Create manual-service-em.js

---
+++ pages/manual-service-em.js
@@ -0,0 +1,18 @@
+  
+import React from 'react';
+import Navbar from '../components/_App/Navbar';
+
+
+const Faq = () => {
+    return (
+        <React.Fragment>
+            <Navbar />
+    
+           
+            <h4>EM</h4>
+            <Footer />
+        </React.Fragment>
+    );
+}
+
+export default Faq;
